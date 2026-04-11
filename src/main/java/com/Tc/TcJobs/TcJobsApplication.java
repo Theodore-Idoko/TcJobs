2a656adb3ffe2c3fd61cf8d43031a245aecd187c
Theodore-Idoko <idokotheodore@gmail.com>
2026-04-11 12:57:34 +0200

    first commit

---
+++ src/main/java/com/Tc/TcJobs/TcJobsApplication.java
@@ -0,0 +1,13 @@
+package com.Tc.TcJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tc/TcJobs/TcJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tc.TcJobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
