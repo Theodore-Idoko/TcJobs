f4937f954f1919f84ab723973e1416d3d90d602e
Theodore-Idoko <idokotheodore@gmail.com>
2026-04-26 20:25:20 +0200

    create new jobs and upload to the dashboard

---
+++ src/main/java/com/Tc/TcJobs/services/JobPostActivityService.java
@@ -0,0 +1,33 @@
+package com.Tc.TcJobs.services;
+
+import com.Tc.TcJobs.entity.*;
+import com.Tc.TcJobs.repository.JobPostActivityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobPostActivityService {
+    private final JobPostActivityRepository jobPostActivityRepository;
+    public JobPostActivityService(JobPostActivityRepository jobPostActivityRepository) {
+        this.jobPostActivityRepository = jobPostActivityRepository;
+    }
+
+    public JobPostActivity  addNew(JobPostActivity jobPostActivity){
+        return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter ){
+        List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+
+        for(IRecruiterJobs rec: recruiterJobsDtos){
+            JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(),rec.getState(),rec.getCountry());
+            JobCompany comp = new JobCompany(rec.getCompanyId(),rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(), rec.getJob_title(),loc, comp));
+
+        }
+        return recruiterJobsDtoList;
+    }
+}
